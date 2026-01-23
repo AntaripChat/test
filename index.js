516508457d18d6ee452992b6b2725f8cad145143
Antarip Chatterjee <antaripchatterjee@gmail.com>
2026-01-23 21:46:05 +0530

    add routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const arycore = require('aryacore');
+const testRouts = require('./routes/test.route');
 
 const app = arycore(); 
 
 app.get('/', (req, res) => {
   res.send('Hello, World! Antarip');
 });
+testRouts(app);
 
 
 const PORT = process.env.PORT || 3000;
